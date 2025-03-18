84e3f86e1fd76bb5effdf8a1ee44d082490d833d
Raphael Schütz <70175424+Raphael2b3@users.noreply.github.com>
2025-03-18 15:48:54 +0100

    Stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const cats = {
+  'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+  'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+};
+
 export function activate(context: vscode.ExtensionContext) {
+  // Track the current panel with a webview
+  let currentPanel: vscode.WebviewPanel | undefined = undefined;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "folder2graph" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand('catCoding.start', () => {
+      const columnToShowIn = vscode.window.activeTextEditor
+        ? vscode.window.activeTextEditor.viewColumn
+        : undefined;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('folder2graph.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from folder2graph!');
-	});
+      if (currentPanel) {
+        // If we already have a panel, show it in the target column
+        currentPanel.reveal(columnToShowIn);
+      } else {
+        // Otherwise, create a new panel
+        currentPanel = vscode.window.createWebviewPanel(
+          'catCoding',
+          'Cat Coding',
+          columnToShowIn || vscode.ViewColumn.One,
+          {}
+        );
+        currentPanel.webview.html = getWebviewContent('Coding Cat');
 
-	context.subscriptions.push(disposable);
+        // Reset when the current panel is closed
+        currentPanel.onDidDispose(
+          () => {
+            currentPanel = undefined;
+          },
+          null,
+          context.subscriptions
+        );
+      }
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent(cat: keyof typeof cats) {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+    <img src="${cats[cat]}" width="300" />
+</body>
+</html>`;
+}
